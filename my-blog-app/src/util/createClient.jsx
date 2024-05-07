d66ea0c152971a6b342b4da22ec1bcdc87f82924
Meire Aparecida Bezerra Ribeiro <meire.bezerra@hotmail.com>
2024-05-07 11:44:48 -0300

    testes

--- my-blog-app/src/util/createClient.jsx
+++ my-blog-app/src/util/createClient.jsx
@@ -1,8 +1,8 @@
 import * as contentful from 'contentful';
 
 export const client = contentful.createClient({
-    space: import.meta.env.VITE_SPACE_ID,
-    accessToken: import.meta.env.VITE_ACCESS_TOKEN
+    space: "fit873cgelym",//import.meta.env.VITE_SPACE_ID,
+    accessToken: "A_WvGSZvAwvkqSlILCThWGaa65EfGrtc3cph2FVCBUA"//import.meta.env.VITE_ACCESS_TOKEN
 });
 
 
